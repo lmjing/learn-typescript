39480c56d506cf19ad8f5be04bad43b014dfdc77
lmjing <lmjing4905@gmail.com>
2021-07-29 22:24:46 +0900

    5. operator - union & intersection

--- class-note/5_operator.ts
+++ class-note/5_operator.ts
@@ -28,8 +28,21 @@ interface Person {
   age: number;
 }
 
+// 코드는 Developer | Person 중 어떤 타입이 들어올지 모르므로
+// interface Union Type의 경우 공통된 속성에 대해서만 접근이 가능함
+function askSomeoneByUnion(someone: Developer | Person) {
+  someone.name;
+  // someone.age;
+  // someone.skill;
+}
+
+// intersection Type
+// Developer와 Person의 property들이 모두 정의된 Type이 들어올 수 있다.
 function askSomeone(someone: Developer & Person) {
   someone.name;
   someone.age;
   someone.skill;
 }
+// 즉, Developer 나 Person만 들어오는 경우 에러가 발생됨
+// askSomeone({name: '개발자', age: 28});
+askSomeone({name: '개발자', age: 28, skill: '웹 개발'});
