a5d692cd07158215c7bafb94757ef2ccb0748b8f
lmjing <lmjing4905@gmail.com>
2021-07-31 13:04:40 +0900

    7. class

--- class-note/6_enum.ts
+++ class-note/6_enum.ts
@@ -1,10 +1,14 @@
+// 숫자형 이넘: 선언할 때 초기 값을 주면 초기 값부터 차례로 1씩 증가
+// 선언 안하면 0부터 시작함
 enum NumEnum {
-  Zero,
-  One,
+  // Zero,
+  One = 1,
+  Two
 }
 
-console.log(NumEnum.Zero) // 0
+console.log(NumEnum.One) // 1
 
+// 문자형 이넘 - 모든 값에 대한 정의가 필요함
 enum Shoes {
   Nike = '나이키',
   Adidas = '아디다스'
@@ -32,3 +36,9 @@ askQuestion('Yes');
 // askQuestion('예스');
 // askQuestion('y');
 // askQuestion('Yes');
+
+// 복합 이넘도 가능 - 단, 웬만하면 같은 타입으로만 선언할 것
+enum BooleanLikeHeterogeneousEnum {
+  No = 0,
+  Yes = "YES",
+}
--- class-note/class.js
+++ class-note/class.js
@@ -4,6 +4,8 @@ function Person(name, age) {
 }
 var capt = new Person('캡틴', 100);
 
+// Java 등의 객체지향에 더 익숙한 사람들을 위해 새로 생성된 개념으로
+// babel등으로 돌리면 실제 위와 같이 기존 JS 기능을 사용해 개발된 것을 알 수 있다.
 class Person {
   // 클래스 로직
   constructor(name, age) {
