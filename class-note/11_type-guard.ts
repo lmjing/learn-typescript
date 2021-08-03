2b43319b51d79439787bb2987b446883fd085d6b
lmjing <lmjing4905@gmail.com>
2021-08-03 21:21:17 +0900

    11. type guard

--- class-note/11_type-guard.ts
+++ class-note/11_type-guard.ts
@@ -8,13 +8,15 @@ interface Person {
   age: number;
 }
 
+// 타입 단언이 필요한 이유(일반  사용시)
 function introduce(): Developer | Person {
   return { name: 'Tony', age: 33, skill: 'Iron Making' }
 }
-var tony = introduce();
-console.log(tony.skill);
+var tony = introduce(); // union Type이다. (Developer | Person)
+console.log(tony.skill); // 따라서, Developer에만 있는 skill을 쓸 수 없다. (확정X)
 
-if ((tony as Developer).skill) {
+// 해결책 - 타입 단언으로 사용 - 단점: 복잡함.
+if ((tony as Developer).skill) { 
   var skill = (tony as Developer).skill;
   console.log(skill);
 } else if ((tony as Person).age) {
@@ -22,13 +24,14 @@ if ((tony as Developer).skill) {
   console.log(age); 
 }
 
-// 타입 가드 정의
+// 해결책 - 타입 가드 정의 - 명확함
+// isDeveloper -> 내부 property의 값의 유무로 interface Type 체크
 function isDeveloper(target: Developer | Person): target is Developer {
   return (target as Developer).skill !== undefined;
 }
 
 if (isDeveloper(tony)) {
-  console.log(tony.skill);
+  console.log(tony.skill); // Developer이므로 바로 skill에 접근 가능
 } else {
-  console.log(tony.age);
+  console.log(tony.age); // Person이므로 바로 age에 접근 가능
 }
